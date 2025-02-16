90ab45551410c0012c176bc80b5a29740074d8c4
tommy <tommyc98@yahoo.it>
2025-02-16 23:46:09 +0100

    Update API calls in statisticApi and MatchHistoryPage to use API_URL constant

--- frontend/meeplestats/src/api/statisticApi.ts
+++ frontend/meeplestats/src/api/statisticApi.ts
@@ -1,6 +1,8 @@
+import { API_URL } from "../model/Constants";
+
 export const fetchStatistics = async (endpoint: string, params: Record<string, string> = {}) => {
   const queryString = new URLSearchParams(params).toString();
-  const response = await fetch(`/api/${endpoint}?${queryString}`, {
+  const response = await fetch(`${API_URL}/${endpoint}?${queryString}`, {
     method: "GET",
     credentials: "include",
   });
